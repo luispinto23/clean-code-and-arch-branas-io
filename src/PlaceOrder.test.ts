e7ab8d55a7ddf3daef434d806ae0ea1a42c4435c
Luís Pinto <luispinto23@gmail.com>
2021-07-29 23:33:56 +0100

    test: should not accept expired coupon

--- src/PlaceOrder.test.ts
+++ src/PlaceOrder.test.ts
@@ -28,3 +28,30 @@ test('should post an Order', () => {
   const output = placeOrder.execute(input);
   expect(output.total).toBe(5672);
 });
+
+test('should not allow an Order with an expired coupon', () => {
+  const input = {
+    cpf: '778.278.412-36',
+    items: [
+      {
+        description: 'Guitar',
+        price: 1000,
+        quantity: 2
+      },
+      {
+        description: 'Amplifier',
+        price: 5000,
+        quantity: 1
+      },
+      {
+        description: 'Cable',
+        price: 30,
+        quantity: 3
+      }
+    ],
+    coupon: 'VALE25'
+  };
+  const placeOrder = new PlaceOrder();
+
+  expect(() => placeOrder.execute(input)).toThrow(new Error('Expired coupon'));
+});
